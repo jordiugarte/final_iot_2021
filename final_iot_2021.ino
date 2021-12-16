3cd5c4eef4711c848cf9143560edbcdb4abb2ecd
Jordi Ugarte <jordi933@gmail.com>
2021-12-16 14:44:34 -0400

    wifi

--- final_iot_2021.ino
+++ final_iot_2021.ino
@@ -1,6 +1,29 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ 
+*/
+#include <WiFi.h>
+#include <PubSubClient.h>
 
+const String MQTT_ADDRESS = "";
+const char* ssid = "";
+const char* password = "";
+
+void setup() {
+  Serial.begin(115200);
+  wifiConnection();
+    
+}
+
+void wifiConnection() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
